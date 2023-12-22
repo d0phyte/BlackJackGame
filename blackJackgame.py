ea0ef238142e8228d09e569866d9767c88e2d1c9
d0phyte <74654976+d0phyte@users.noreply.github.com>
2023-12-22 18:51:10 +0000

    Fixed a few bugs and made it so the user can keep playing turn after turn

--- blackJackgame.py
+++ blackJackgame.py
@@ -5,9 +5,10 @@ import random
 #11 is the Ace card
 #10 are the queen/king/jack and 10 cards
 #Ace can also be 1 but for now will just use 11
-#There is a bug where if the computer gets 21 it wins before the user can draw a card: fixed
-#Bug where if the user stands, if the computer is not over 16 the last card is not added to the sum: not fixed
 
+#There is a bug where if the computer gets 21 it wins before the user can draw a card: fixed
+#Bug where if the user stands, if the computer is not over 16 the last card is not added to the sum: fixed
+#If the user has 21 points he should not be able to draw another card (should automatically stand): not fixed yet
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10] 
 
@@ -16,6 +17,25 @@ userCards = []
 
 #computerScore = 0
 #userScore = 0
+
+#This function is called at the end of the game to see if the user wants to play again
+
+def playAgain():
+    playAgain = input('Do you want to play again? Type "y" or "n": ')
+    if playAgain == 'y':
+        computerCards.clear()
+        userCards.clear()
+        gameStart()
+    elif playAgain == 'n':
+        print('Thanks for playing!')
+        return
+    else:
+        print('Invalid input! Stopping game!')
+
+        return
+        
+        
+
 
 #The computer is forced to draw until its score is over 16
 
@@ -26,9 +46,14 @@ def checkComputerOver16(computerScore, userScore):
         newCardComputer = random.choice(cards)
         computerCards.append(newCardComputer)
         computerScore = sum(computerCards)
-        print(f'The computer score is lower than 16 so it draws extra card/cards. It draws a {newCardComputer} ')
+        print(f'The computer draws a {newCardComputer} ')
     
-    return computerScore
+    if i == 0:
+        #print('The computer did not draw any extra cards because it was already over 16 points.')
+        return computerScore
+    else:
+        print(f'The computer drew {i} extra cards.')
+        return computerScore
         
 
 
@@ -38,27 +63,35 @@ def checkWinner(computerScore, userScore):
     
         if computerScore == 21 and userScore == 21:
             print('It is a tie!')
+            playAgain()
             return 
         elif computerScore == 21 and userScore < 21:
             print('The computer wins!')
+            playAgain()
             return
         elif userScore == 21 and computerScore < 21 :
             print('The user wins!')
+            playAgain()
             return
         elif computerScore > 21:
             print('The computer is over 21! It loses! ')
+            playAgain()
             return
         elif userScore > 21:
             print('The user is over 21! It loses!')
+            playAgain() 
             return
         elif computerScore > userScore:
             print('The computer wins!')
+            playAgain()
             return
         elif userScore > computerScore:
             print('The user wins!')
+            playAgain()
             return
         elif computerScore == userScore:
             print('It is a tie!')
+            playAgain() 
             return
         
         else:
@@ -70,8 +103,8 @@ def drawOneCard(computerScore, userScore):
     #checkWinner(computerScore, userScore)
 
     if userScore < 22:
-        
-        
+      
+
         hitOrStand = input('Do you want to hit or stand? Type "h" or "s": ')
         
         if hitOrStand == 'h':
@@ -82,19 +115,23 @@ def drawOneCard(computerScore, userScore):
             print(f'The user draws a {newDraw}')
             print(f'The computer has {computerScore} points and the user has {userScore} points.')
             drawOneCard(computerScore, userScore)
-        
+            
 
         elif hitOrStand == 's':
             print('The user stands!')
             computerScore = checkComputerOver16(computerScore, userScore)
             print(f'The computer has {computerScore} points and the user has {userScore} points.')
             checkWinner(computerScore, userScore)
+            
+            
+            
         
         else:
-            print('Invalid input!')
+            print('Invalid input! Try again!')
             drawOneCard(computerScore, userScore)
     else:
         print('The user is over 21! Game Over!')
+        playAgain()
         return
 
 
@@ -126,9 +163,10 @@ def gameStart():
 
 
 gameStart()
-            
-        
 
+
+            
+       
 
 
 
